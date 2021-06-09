77dc579dc74dab4bb1ef9f90ca92be03d406894c
Aman Tandon <aman.tandon3096@gmail.com>
2021-06-09 18:42:21 +0530

    fix: service worker

--- push.js
+++ push.js
@@ -6,7 +6,7 @@ let vapidKeys= {
 
   push.setVapidDetails('mailto: test@test@code.com', vapidKeys.publicKey, vapidKeys.privateKey)
 
-  let sub = {};
+  let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/cDyjUOb47kg:APA91bEgbd10e3OdLXKYT-WoWjQd7iHQygvZ97YJ5yF1wlnAA6rSJbY-jByTvB3aFtH4EH7EW_dVMOlVp6OGP037g5D6m05lTZ-Ay6I__sPUwkT3kQ8GzkBbrU7ocHH0TJzrin1gy20x","expirationTime":null,"keys":{"p256dh":"BPPB9e5l1fYNyhA9kuWULB-UBqR7Eqs8W3rvvXB4wDmEkDvkd2YXetCx9Fc_c71rZIlwQwl8BRqm_G0hjsH_E6M","auth":"-kQ0KJmqxIpuIFmcM46t2A"}}
 
   push.sendNotification( sub, 'test message')
   
