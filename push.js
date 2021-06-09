5b37830d06b240eb8f49b262f8b9598cc51a896e
Aman Tandon <aman.tandon3096@gmail.com>
2021-06-09 19:13:13 +0530

    fix keys

--- push.js
+++ push.js
@@ -6,8 +6,8 @@ let vapidKeys= {
   privateKey: 'i9hF-Snk9jE2nK9j1to_DRRPzeIgxfFlqROkAntVCyA'
 }
 
-  push.setVapidDetails('mailto: test@test@code.com', vapidKeys.publicKey, vapidKeys.privateKey)
+  push.setVapidDetails('mailto:test@test@code.com', vapidKeys.publicKey, vapidKeys.privateKey)
 
-  let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/cDyjUOb47kg:APA91bEgbd10e3OdLXKYT-WoWjQd7iHQygvZ97YJ5yF1wlnAA6rSJbY-jByTvB3aFtH4EH7EW_dVMOlVp6OGP037g5D6m05lTZ-Ay6I__sPUwkT3kQ8GzkBbrU7ocHH0TJzrin1gy20x","expirationTime":null,"keys":{"p256dh":"BPPB9e5l1fYNyhA9kuWULB-UBqR7Eqs8W3rvvXB4wDmEkDvkd2YXetCx9Fc_c71rZIlwQwl8BRqm_G0hjsH_E6M","auth":"-kQ0KJmqxIpuIFmcM46t2A"}}
+  let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/cDyjUOb47kg:APA91bEgbd10e3OdLXKYT-WoWjQd7iHQygvZ97YJ5yF1wlnAA6rSJbY-jByTvB3aFtH4EH7EW_dVMOlVp6OGP037g5D6m05lTZ-Ay6I__sPUwkT3kQ8GzkBbrU7ocHH0TJzrin1gy20x","expirationTime":null,"keys":{"p256dh":"BPPB9e5l1fYNyhA9kuWULB-UBqR7Eqs8W3rvvXB4wDmEkDvkd2YXetCx9Fc_c71rZIlwQwl8BRqm_G0hjsH_E6M","auth":"-kQ0KJmqxIpuIFmcM46t2A"}};
   push.sendNotification( sub, 'test message')
   
